f05d0f124f929c9eec4f228abf2d4065351e674c
jayhitemup <johanpena02@outlook.com>
2017-06-29 20:30:04 +0000

    the half-finished version of the project

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 $(document).ready(function() {
 
-   
-    
+    var name = $("#name").val();
+    var email = $("#name").val();
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("signbtn");
+    var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+$(".col-sm-6").mouseover(function(){
+    $(this).animate({
+        paddingTop: "15px"
+    }, 100);
+}).mouseout(function(){
+    $(this).animate({
+        paddingTop: "10px"
+    }, 100);
+});
 
 });
